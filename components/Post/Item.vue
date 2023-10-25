31d05efeef66fc60132d26476efd03d276d699ed
takeotky <d.noszczyk@vultive.com>
2023-10-25 18:39:03 +0200

    style: adjust post image size

--- components/Post/Item.vue
+++ components/Post/Item.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 
 <template>
     <div class="uk-panel">
-        <img :src="post._embedded['wp:featuredmedia']['0']?.source_url" width="1800" height="1200"
+        <img :src="post._embedded['wp:featuredmedia']['0']?.source_url" width="1280" height="720"
             :alt="post.title?.rendered">
         <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
             <h4 class="uk-margin-remove uk-text-bold uk-text-nowrap uk-text-italic uk-overflow-hidden uk-text-truncate">{{
