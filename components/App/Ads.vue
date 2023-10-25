67ad58d534646cf7a49115ef1ae3b987f990a801
takeotky <d.noszczyk@vultive.com>
2023-10-25 23:37:30 +0200

    chore: temporarily comment out ads

--- components/App/Ads.vue
+++ components/App/Ads.vue
@@ -1,4 +1,4 @@
 <template>
-    <Adsbygoogle ad-slot="8269055061" :ad-style="{ display: 'inline-block', width: '728px', height: '140px' }"
-        class="uk-margin-medium" />
+    <!-- <Adsbygoogle ad-slot="8269055061" :ad-style="{ display: 'inline-block', width: '728px', height: '140px' }"
+        class="uk-margin-medium" /> -->
 </template>
